ea4855cc4b39591503f99b8a03c79d3f63c912fd
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-01-16 21:27:12 +0000

    Test: Create HomeScreen unit test

---
+++ test/screens/home_screen_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping_list_but_free/screens/home_screen.dart';
+
+void main() {
+  group('HomeScreen', () {
+    group('static', () {
+      testWidgets('Display navigation menu', (tester) async {
+        await tester.pumpWidget(getHomeScreen());
+        expect(find.byTooltip('Open navigation menu'), findsOneWidget);
+      });
+
+      testWidgets('Dispaly shopping list adding button', (tester) async {
+        await tester.pumpWidget(getHomeScreen());
+        expect(find.byType(FloatingActionButton), findsOneWidget);
+      });
+
+      testWidgets('Display correct title', (tester) async {
+        await tester.pumpWidget(getHomeScreen());
+        expect(find.text('Shopping List But Free'), findsOneWidget);
+      });
+    });
+  });
+}
+
+Widget getHomeScreen() => MaterialApp(home: const HomeScreen());
