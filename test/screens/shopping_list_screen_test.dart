869f8d89fae5c0487f0e6d0eb520c65d15044998
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-01-30 15:34:43 +0000

    Test: Add popup menu tests to ShoppingListScreen

--- test/screens/shopping_list_screen_test.dart
+++ test/screens/shopping_list_screen_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:shopping_list_but_free/models/shopping_item.dart';
 import 'package:shopping_list_but_free/models/shopping_list.dart';
 import 'package:shopping_list_but_free/objectbox.dart';
 import 'package:shopping_list_but_free/screens/shopping_list_screen.dart';
@@ -14,6 +15,7 @@ void main() async {
 
   late ShoppingList shoppingList;
 
+  /// Empties database and populates it
   void setUp(Function populate) {
     // Empty database
     objectbox.shoppingListBox.removeAll();
@@ -23,6 +25,13 @@ void main() async {
     // Populate database
     populate();
   }
+
+  Widget _getShoppingListScreen() => MaterialApp(
+        theme: ThemeData(useMaterial3: true),
+        home: ShoppingListScreen(
+          shoppingList: shoppingList,
+        ),
+      );
 
   group(
     'ShoppingListScreen',
@@ -41,7 +50,7 @@ void main() async {
               });
 
               // Pump ShoppingListScreen
-              await tester.pumpWidget(_getShoppingListScreen(shoppingList));
+              await tester.pumpWidget(_getShoppingListScreen());
 
               // Test for correct title
               expect(find.text('Test Shopping List'), findsOneWidget);
@@ -59,10 +68,105 @@ void main() async {
               });
 
               // Pump ShoppingListScreen
-              await tester.pumpWidget(_getShoppingListScreen(shoppingList));
+              await tester.pumpWidget(_getShoppingListScreen());
 
               // Test for correct title
               expect(find.byTooltip('Open navigation menu'), findsOneWidget);
+            },
+          );
+
+          group(
+            'Popup Menu',
+            () {
+              Future<void> popUpMenuSetup(WidgetTester tester) async {
+                // Set shoppingList
+                shoppingList = ShoppingList(name: 'Test Shopping List');
+
+                setUp(() {
+                  shoppingList.shoppingItems
+                      .add(ShoppingItem(name: 'Test Shopping Item 1'));
+                  shoppingList.shoppingItems
+                      .add(ShoppingItem(name: 'Test Shopping Item 2'));
+                  objectbox.shoppingListBox.put(shoppingList);
+                });
+
+                // Pump ShoppingListScreen
+                await tester.pumpWidget(_getShoppingListScreen());
+
+                // Tap popup menu
+                await tester.tap(find.byTooltip('Show menu'));
+                await tester.pumpAndSettle();
+              }
+
+              testWidgets(
+                'Display popup menu',
+                (tester) async {
+                  // Set shoppingList
+                  shoppingList = ShoppingList(name: 'Test Shopping List');
+
+                  setUp(() {
+                    objectbox.shoppingListBox.put(shoppingList);
+                  });
+
+                  // Pump ShoppingListScreen
+                  await tester.pumpWidget(_getShoppingListScreen());
+
+                  // Test for correct title
+                  expect(find.byTooltip('Show menu'), findsOneWidget);
+                },
+              );
+
+              testWidgets(
+                'Tapping on popup menu displays delete option',
+                (tester) async {
+                  await popUpMenuSetup(tester);
+
+                  // Test for delete option
+                  expect(find.text('Delete'), findsOneWidget);
+                },
+              );
+
+              testWidgets(
+                'Tapping on delete option deletes all shopping items in list',
+                (tester) async {
+                  await popUpMenuSetup(tester);
+
+                  // Tap delete option
+                  await tester.tap(find.text('Delete'));
+                  await tester.pumpAndSettle();
+
+                  // Test for shopping items in database
+                  expect(objectbox.shoppingItemBox.isEmpty(), true);
+                },
+              );
+
+              testWidgets(
+                'Tapping on delete option deletes shopping list',
+                (tester) async {
+                  await popUpMenuSetup(tester);
+
+                  // Tap delete option
+                  await tester.tap(find.text('Delete'));
+                  await tester.pumpAndSettle();
+
+                  // Test for shopping list in database
+                  expect(objectbox.shoppingListBox.get(shoppingList.id), null);
+                },
+              );
+
+              testWidgets(
+                'Tapping on delete option pops screen',
+                (tester) async {
+                  await popUpMenuSetup(tester);
+
+                  // Tap delete option
+                  await tester.tap(find.text('Delete'));
+                  await tester.pumpAndSettle();
+
+                  // Test for absence of ShoppingListScreen
+                  expect(find.byType(ShoppingListScreen), findsNothing);
+                },
+              );
             },
           );
         },
@@ -70,13 +174,6 @@ void main() async {
     },
   );
 }
-
-Widget _getShoppingListScreen(ShoppingList shoppingList) => MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: ShoppingListScreen(
-        shoppingList: shoppingList,
-      ),
-    );
 
 class FakePathProviderPlatform extends Fake
     with MockPlatformInterfaceMixin
