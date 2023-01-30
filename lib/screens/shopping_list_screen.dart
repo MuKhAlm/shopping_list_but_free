b37a75d23dafc439a3a08fd4794c7625e41db27f
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-01-30 15:36:14 +0000

    Dev: Add functional popup menu to ShoppingListScreen
    Effects:
        - ShoppingListScreen can now be deleted from the popup menu.

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_but_free/models/shopping_list.dart';
+import 'package:shopping_list_but_free/objectbox.dart';
 
 class ShoppingListScreen extends StatefulWidget {
   final ShoppingList shoppingList;
@@ -16,9 +17,40 @@ class ShoppingListScreen extends StatefulWidget {
 class _ShoppingListScreenState extends State<ShoppingListScreen> {
   @override
   Widget build(BuildContext context) {
+    var shoppingList = widget.shoppingList;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.shoppingList.name),
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) {
+              if (value == 'delete') {
+                // Delete shopping items
+                if (shoppingList.shoppingItems.isNotEmpty) {
+                  for (var shoppingItem in shoppingList.shoppingItems) {
+                    objectbox.shoppingItemBox.remove(shoppingItem.id);
+                  }
+                }
+                // Delete shopping list
+                objectbox.shoppingListBox.remove(shoppingList.id);
+                // Pop route
+                Navigator.of(context).pop();
+              }
+            },
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 'delete',
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Icon(Icons.delete_forever_outlined),
+                    Text('Delete'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
       drawer: const Drawer(),
       body: const SafeArea(
