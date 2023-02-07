33bc5e8bb0d866a47f68806e2dae8dcd372336be
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2023-02-07 21:16:54 +0000

    Dev: Partially implement ChangeCollection
    Effects:
        - ChangeCollection now displays a form for the user to select an one
        of the existing Collections to change all Shopping Items with the same
        name to (NOT CASE SENSITIVE).
    
    Notes:
        - An assertion error occasionally occurs after a few collection change
        operations, however, it is speculated that the fault lays in the logic
        that expands and retracts collection panels in ShoppingListScreen and
        not in ChangeCollection.

---
+++ lib/widgets/change_collection.dart
@@ -0,0 +1,124 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shopping_list_but_free/models/collection.dart';
+import 'package:shopping_list_but_free/models/shopping_item.dart';
+import 'package:shopping_list_but_free/objectbox.dart';
+import 'package:shopping_list_but_free/objectbox.g.dart';
+
+/// Displays a **card** containing a **form**,
+///
+/// Selects a already-existing or new **Collection** for [shoppingItem],
+///
+/// Changes corresponding collection for all ShoppingItem with the same name as [shoppingItem]
+/// (**NOT CASE SENSITIVE**) across the app.
+class ChangeCollection extends StatefulWidget {
+  final ShoppingItem shoppingItem;
+
+  const ChangeCollection({
+    required this.shoppingItem,
+    super.key,
+  });
+
+  @override
+  State<ChangeCollection> createState() => _ChangeCollectionState();
+}
+
+class _ChangeCollectionState extends State<ChangeCollection> {
+  late Collection _selectedCollection;
+  List<Collection> collections = objectbox.collectionBox.getAll();
+
+  @override
+  void initState() {
+    super.initState();
+    // Picks an initial collection
+    setState(() {
+      _selectedCollection = collections[0];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: min(MediaQuery.of(context).size.width - 10, 300),
+        height: MediaQuery.of(context).size.height / 3,
+        child: Card(
+          elevation: 20,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  BackButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+              // Forces Padding to take all available space
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Form(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        DropdownButtonFormField<Collection>(
+                          value: _selectedCollection,
+                          items: collections
+                              .map(
+                                (collection) => DropdownMenuItem(
+                                  value: collection,
+                                  child: Text(collection.name),
+                                ),
+                              )
+                              .toList(),
+                          onChanged: (value) {
+                            if (value != null) {
+                              setState(() {
+                                _selectedCollection = value;
+                              });
+                            }
+                          },
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            final String shoppingItemName =
+                                widget.shoppingItem.name.toLowerCase();
+                            final Collection currentCollection = objectbox
+                                .collectionBox
+                                .query(Collection_.shoppingItemsNames
+                                    .containsElement(shoppingItemName))
+                                .build()
+                                .findFirst() as Collection;
+                            final Collection newCollection =
+                                _selectedCollection;
+
+                            // Remove shoppingItem name from current Collection that
+                            // has it
+                            currentCollection.shoppingItemsNames
+                                .remove(shoppingItemName);
+                            // Add shoppingItem name to selected Collection
+                            newCollection.shoppingItemsNames
+                                .add(shoppingItemName);
+
+                            // Put both Collections in obx to rebuild UI
+                            objectbox.collectionBox
+                                .putMany([currentCollection, newCollection]);
+
+                            // Pop Widget
+                            Navigator.of(context).pop();
+                          },
+                          icon: const Icon(Icons.done),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
